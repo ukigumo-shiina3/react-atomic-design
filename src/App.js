a70d4a7512af1ee2dd7ef5c5393315336cb4bbff
daisuke33 <67767613+ukigumo-shiina3@users.noreply.github.com>
2021-12-28 11:50:40 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { PrimaryButton } from "./components/atoms/button/Primarybutton";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PrimaryButton>テスト</PrimaryButton>
+      <PrimaryButton>検索</PrimaryButton>
+    </div>
+  );
+}
