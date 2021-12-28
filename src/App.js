faa176a71a0c4c64912630f6ab4b51a4274e50a4
daisuke33 <67767613+ukigumo-shiina3@users.noreply.github.com>
2021-12-28 12:20:49 +0900

    organismsを作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "テスト",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "12345@gmail.com",
+  phone: "090-1234-5678",
+  company: {
+    name: "aaa株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
